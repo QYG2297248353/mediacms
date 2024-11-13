736010c08447692bf6de34ebf55a9f986d0c7744
Meng Sen <qyg2297248353@gmail.com>
2024-11-13 14:02:05 +0800

    Update local_settings.py

--- deploy/docker/local_settings.py
+++ deploy/docker/local_settings.py
@@ -1,20 +1,25 @@
-FRONTEND_HOST = 'http://localhost'
-PORTAL_NAME = 'MediaCMS'
-SECRET_KEY = 'ma!s3^b-cw!f#7s6s0m3*jx77a@riw(7701**(r=ww%w!2+yk2'
-POSTGRES_HOST = 'db'
-REDIS_LOCATION = "redis://redis:6379/1"
+import os
 
+# Fetch environment variables with default values
+FRONTEND_HOST = os.getenv('FRONTEND_HOST', 'http://localhost')
+PORTAL_NAME = os.getenv('PORTAL_NAME', 'MediaCMS')
+SECRET_KEY = os.getenv('SECRET_KEY', 'ma!s3^b-cw!f#7s6s0m3*jx77a@riw(7701**(r=ww%w!2+yk2')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'db')
+REDIS_LOCATION = os.getenv('REDIS_LOCATION', 'redis://redis:6379/1')
+
+# Database settings
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mediacms",
+        "NAME": os.getenv('DB_NAME', 'mediacms'),
         "HOST": POSTGRES_HOST,
-        "PORT": "5432",
-        "USER": "mediacms",
-        "PASSWORD": "mediacms",
+        "PORT": os.getenv('DB_PORT', '5432'),
+        "USER": os.getenv('DB_USER', 'mediacms'),
+        "PASSWORD": os.getenv('DB_PASSWORD', 'mediacms'),
     }
 }
 
+# Caching settings
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
@@ -29,6 +34,6 @@ CACHES = {
 BROKER_URL = REDIS_LOCATION
 CELERY_RESULT_BACKEND = BROKER_URL
 
-MP4HLS_COMMAND = "/home/mediacms.io/bento4/bin/mp4hls"
+MP4HLS_COMMAND = os.getenv('MP4HLS_COMMAND', '/home/mediacms.io/bento4/bin/mp4hls')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
